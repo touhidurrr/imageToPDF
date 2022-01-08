0c1893f62a5f4708b4d2314641d6b448e808cf7f
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2022-01-09 05:31:30 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 npm install
+mkdir lib
 npx browserify -s PDFDocument -i crypto -i iconv-lite node_modules/pdfkit/js/pdfkit.js | npx uglifyjs -cm > lib/pdfkit.js
 npx browserify -s blobStream -p tinyify node_modules/blob-stream/index.js -o lib/blob-stream.js
 rm -rf node_modules

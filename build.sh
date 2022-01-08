f5a60ef36f2daeb10e0b025ee36e5d0d68c2946c
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2022-01-09 04:59:36 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ npm install
 npx browserify -s PDFDocument -i crypto -i iconv-lite node_modules/pdfkit/js/pdfkit.js | npx uglifyjs -cm > lib/pdfkit.js
 npx browserify -s blobStream -p tinyify node_modules/blob-stream/index.js -o lib/blob-stream.js
 rm -rf node_modules
-rm build.sh
+rm -f LICENSE README.md build.sh package.json

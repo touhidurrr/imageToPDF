4f8bf0be7b68892f8e5e39248ca4033a63ab49b9
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2024-10-12 23:09:37 +0600

    fix errors

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+bun install
+
 mkdir public/lib
 bunx --bun browserify -s blobStream node_modules/blob-stream/index.js -o public/lib/blob-stream.js
 bunx --bun browserify -s PDFDocument -i crypto -i iconv-lite node_modules/pdfkit/js/pdfkit.js | bunx --bun uglifyjs -cm > public/lib/pdfkit.js
